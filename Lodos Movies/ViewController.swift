c8040a6d2be6f5a8e50f794cc7cbdc07af3a9828
tugrulhanEvam <tugrulhan@evam.com>
2024-03-12 22:36:00 +0300

    Initial Commit

---
+++ Lodos Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lodos Movies
+//
+//  Created by Tuğrul on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
